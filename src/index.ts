04c0b5a14ccffdf566b9e81f73fa83be8e7deb90
KarunJr <ghimirekarun406@gmail.com>
2026-03-27 17:40:52 +0545

    init

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import "dotenv/config"
+import express from "express";
+const app = express();
+const PORT = 3000;
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on http://localhost:${PORT}`);
+});
